72d668a6929199231c879ad6a26d831869dc56ec
Nicotinii <nml.lefevre@gmail.com>
2022-11-29 15:13:46 +0100

    JS_CHALL4

--- script.js
+++ script.js
@@ -1,74 +1,39 @@
 'strict mode';
 
-let BernardA = {
-    weight: 78,
-    height: 1.69
+let dauphins1 = [96, 108, 89];
+let koalas1 = [88, 91, 110];
+
+let dauphins2 = [97, 112, 101];
+let koalas2 = [109, 95, 123];
+
+let dauphins3 = [97, 112, 101];
+let koalas3 = [109, 95, 106];
+
+
+function average (score) {
+    let totalscore = score.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+    let cbscore = score.length;
+    return parseFloat(totalscore / cbscore).toFixed(2);
 };
 
-let MarcelA = {
-    weight: 92,
-    height: 1.95
-};
 
-let BernardB = {
-    weight: 95,
-    height: 1.88
-};
-
-let MarcelB = {
-    weight: 85,
-    height: 1.76
-};
-
-function imc (weight, height) {
-    return (weight / (height * height)).toFixed(2);
-};
-
-let imcBernardA = imc(BernardA.weight, BernardA.height);
-let imcMarcelA = imc(MarcelA.weight, MarcelA.height);
-
-let imcBernardB = imc(BernardB.weight, BernardB.height);
-let imcMarcelB = imc(MarcelB.weight, MarcelB.height);
-
-let messageA = "";
-let messageB = "";
-
-if (imcBernardA > imcMarcelA) {
-    messageA = "Bernard a un IMC(" + imcBernardA + ") plus élevé que Marcel(" + imcMarcelA + ").";
-} else {
-    messageA = "Marcel a un IMC(" + imcMarcelA + ") plus élevé que Bernard(" + imcBernardA + ").";
-}
-
-if (imcBernardB > imcMarcelB) {
-    messageB = "Bernard a un IMC(" + imcBernardB + ") plus élevé que Marcel(" + imcMarcelB + ").";
-} else {
-    messageB = "Marcel a un IMC(" + imcMarcelB + ") plus élevé que Bernard(" + imcBernardB + ").";
-}
-
-console.log(messageA); //Bernard a un IMC(27.31) plus élevé que Marcel(24.19).
-console.log(messageB); //Marcel a un IMC(27.44) plus élevé que Bernard(26.88).
-
-
-
-
-function imcMessage (nivea) {
-    if (imc(nivea.weight, nivea.height) <20) {
-        return "Insuffisance pondérale";
-    } else if (imc(nivea.weight, nivea.height) <25) {
-        return "Normal";
-    } else if (imc(nivea.weight, nivea.height) <30) {
-        return "Surpoids";
-    } else if (imc(nivea.weight, nivea.height) <40) {
-        return "Obésité";
+function winner (dauphins, koalas) {
+    if ((average(dauphins) || average(koalas)) > 100) {
+        if (average(koalas) < average(dauphins)) {
+            return "Les Dauphins espiegle gagnent avec une moyenne de " + average(dauphins) + " contre seulement" + average(koalas) + " pour les Koalas les nuls";
+        } else if (average(dauphins) < average(koalas)){
+            return "Les Koalas trop fort gagnent avec une moyenne de " + average(koalas) + " contre une moyenne de " + average(dauphins) + " pour les Dauphins case prison";
+        } else {
+            return "Egalité impossible de les départager zut " + average(dauphins);
+        }
     } else {
-        return "Obésité importante";
+        return "tous nul" + " dauphins : " + average(dauphins) + " kaoala : " + average(koalas);
     }
 }
 
-console.log(imcMessage(BernardA)); //Surpoids
-console.log(imcMessage(MarcelA)); //Normal
-console.log(imcMessage(BernardB));//Surpoids
-console.log(imcMessage(MarcelB));//Surpoids
+console.log(winner(dauphins1, koalas1));
+console.log(winner(dauphins2, koalas2));
+console.log(winner(dauphins3, koalas3));
 
 
 
