954d595a16e60e76c70504e6da22976e7c49a5ce
Nicotinii <nml.lefevre@gmail.com>
2022-11-29 10:02:54 +0100

    JS_CHALL2

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 'strict mode';
 
-let admin = name;
-const name = "John";
+let a = 1, b = 1;
+let c = ++a;
+let d = b++;
 
-console.log(admin);
+console.log(a, b, c, d);
+
+// a = 2, b = 2,  c = 2, d = 1
