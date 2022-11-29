8fdc7bac2385f96b27c6c0915e373a365da9368a
Nicotinii <nml.lefevre@gmail.com>
2022-11-29 11:37:10 +0100

    JS_CHALL3

--- script.js
+++ script.js
@@ -1,9 +1,75 @@
 'strict mode';
 
-let a = 1, b = 1;
-let c = ++a;
-let d = b++;
+let BernardA = {
+    weight: 78,
+    height: 1.69
+};
 
-console.log(a, b, c, d);
+let MarcelA = {
+    weight: 92,
+    height: 1.95
+};
 
-// a = 2, b = 2,  c = 2, d = 1
+let BernardB = {
+    weight: 95,
+    height: 1.88
+};
+
+let MarcelB = {
+    weight: 85,
+    height: 1.76
+};
+
+function imc (weight, height) {
+    return (weight / (height * height)).toFixed(2);
+};
+
+let imcBernardA = imc(BernardA.weight, BernardA.height);
+let imcMarcelA = imc(MarcelA.weight, MarcelA.height);
+
+let imcBernardB = imc(BernardB.weight, BernardB.height);
+let imcMarcelB = imc(MarcelB.weight, MarcelB.height);
+
+let messageA = "";
+let messageB = "";
+
+if (imcBernardA > imcMarcelA) {
+    messageA = "Bernard a un IMC(" + imcBernardA + ") plus élevé que Marcel(" + imcMarcelA + ").";
+} else {
+    messageA = "Marcel a un IMC(" + imcMarcelA + ") plus élevé que Bernard(" + imcBernardA + ").";
+}
+
+if (imcBernardB > imcMarcelB) {
+    messageB = "Bernard a un IMC(" + imcBernardB + ") plus élevé que Marcel(" + imcMarcelB + ").";
+} else {
+    messageB = "Marcel a un IMC(" + imcMarcelB + ") plus élevé que Bernard(" + imcBernardB + ").";
+}
+
+console.log(messageA); //Bernard a un IMC(27.31) plus élevé que Marcel(24.19).
+console.log(messageB); //Marcel a un IMC(27.44) plus élevé que Bernard(26.88).
+
+
+
+
+function imcMessage (nivea) {
+    if (imc(nivea.weight, nivea.height) <20) {
+        return "Insuffisance pondérale";
+    } else if (imc(nivea.weight, nivea.height) <25) {
+        return "Normal";
+    } else if (imc(nivea.weight, nivea.height) <30) {
+        return "Surpoids";
+    } else if (imc(nivea.weight, nivea.height) <40) {
+        return "Obésité";
+    } else {
+        return "Obésité importante";
+    }
+}
+
+console.log(imcMessage(BernardA)); //Surpoids
+console.log(imcMessage(MarcelA)); //Normal
+console.log(imcMessage(BernardB));//Surpoids
+console.log(imcMessage(MarcelB));//Surpoids
+
+
+
+
