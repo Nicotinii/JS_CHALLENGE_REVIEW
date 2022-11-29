fb185d5bedf9d85d8e079c133dd331da8d6bf9fb
Nicotinii <nml.lefevre@gmail.com>
2022-11-29 15:36:24 +0100

    JS_CHALL5

--- script.js
+++ script.js
@@ -1,39 +1,22 @@
 'strict mode';
 
-let dauphins1 = [96, 108, 89];
-let koalas1 = [88, 91, 110];
 
-let dauphins2 = [97, 112, 101];
-let koalas2 = [109, 95, 123];
+function bill(facture) {
 
-let dauphins3 = [97, 112, 101];
-let koalas3 = [109, 95, 106];
+let tips = 0;
 
+(50 <= facture <= 300) ? tips = 0.15 * facture : tips = 0.20 * facture
 
-function average (score) {
-    let totalscore = score.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-    let cbscore = score.length;
-    return parseFloat(totalscore / cbscore).toFixed(2);
-};
+let total = facture + tips;
 
+return "La note est de " + facture + " le pourboire est de " + tips + " et la valeur totale était de " + total;
 
-function winner (dauphins, koalas) {
-    if ((average(dauphins) || average(koalas)) > 100) {
-        if (average(koalas) < average(dauphins)) {
-            return "Les Dauphins espiegle gagnent avec une moyenne de " + average(dauphins) + " contre seulement" + average(koalas) + " pour les Koalas les nuls";
-        } else if (average(dauphins) < average(koalas)){
-            return "Les Koalas trop fort gagnent avec une moyenne de " + average(koalas) + " contre une moyenne de " + average(dauphins) + " pour les Dauphins case prison";
-        } else {
-            return "Egalité impossible de les départager zut " + average(dauphins);
-        }
-    } else {
-        return "tous nul" + " dauphins : " + average(dauphins) + " kaoala : " + average(koalas);
-    }
 }
 
-console.log(winner(dauphins1, koalas1));
-console.log(winner(dauphins2, koalas2));
-console.log(winner(dauphins3, koalas3));
+
+console.log(bill(275));
+console.log(bill(40));
+console.log(bill(430));
 
 
 
