16146a8c840310cc2ceee613c3cda4ca873b53cd
Nicotinii <nml.lefevre@gmail.com>
2022-11-30 14:33:57 +0100

    JS_CHALL8

--- script.js
+++ script.js
@@ -7,9 +7,8 @@ function calcTip (facture) {
         return facture * 0.20;
     }
 }
-console.log(calcTip (555))
 
-const bills = [125, 555, 44];
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52]; //[4.4, 44.25, 26.4, 88, 7.4, 15.75, 2, 220, 12.9, 7.8]
 
 let tips = [];
 
@@ -23,5 +22,14 @@ let totals = [];
 for (let i = 0; i < bills.length; i++) {
     totals.push(calcTip(bills[i]) + bills[i]);
 }
+console.log(totals); //[26.4, 339.25, 202.4, 528, 44.4, 120.75, 12, 1320, 98.9, 59.8]
 
-console.log(totals);
+function calcAverage(tab){
+    let tableau = tab.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+    return tableau / tab.length;
+}
+console.log(calcAverage(totals)); // 275.19
+
+
+
+
