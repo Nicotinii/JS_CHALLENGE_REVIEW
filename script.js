ee3a956867497ced0ffaec2e3a3e5e791054909c
Nicotinii <nml.lefevre@gmail.com>
2022-12-01 16:02:46 +0100

    JS_CHALL12

--- script.js
+++ script.js
@@ -3,23 +3,13 @@
 let tempmax1 = [17, 21, 23];
 let tempmax2 = [12, 5, -5, 0, 4];
 
-function alltemp(quecalor) {
-    for (let i = 0; i < quecalor.length; i++) {
-        if (i === 0) {
-            if (quecalor[i] === 0|1) {
-                return meteo = quecalor[i] + " degré dans " + i + 1 + " jour";
-            } else {
-                return meteo = quecalor[i] + "degré dans" + i + 1 + " jour";
-            }
-        }
-        if (i === 0) {
-            if (quecalor[i] === 0|1) {
-                return meteo = quecalor[i] + "degré dans" + i + 1 + " jour";
-            } else {
-                return meteo = quecalor[i] + "degré dans" + i + 1 + " jour";
-            }
-        }
+function tempall (quecalor) {
+    for (let i=0; i<quecalor.length; i++) {
+
+        console.log(quecalor[i] + " degrée dans "  + (i + 1) + " jour" );
+
     }
 }
 
-console.log(alltemp(tempmax1))
+tempall(tempmax1);
+tempall(tempmax2);
